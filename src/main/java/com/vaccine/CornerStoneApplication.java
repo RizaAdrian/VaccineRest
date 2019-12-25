dec77bf9eaad29c511606e9f81c7442a78190aa3
Adrian Riza <rizariza19@gmail.com>
2019-12-25 14:12:38 +0200

    Back-end commited

---
+++ src/main/java/com/vaccine/CornerStoneApplication.java
@@ -0,0 +1,13 @@
+package com.vaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerStoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerStoneApplication.class, args);
+	}
+
+}
